0c8013bf513ed430242f43e5e1e444b6f969ccfe
mudasar187 <mudasar_cheema@hotmail.com>
2025-02-14 10:49:21 +0100

    ikke deploy til prod

--- src/middleware/urls.ts
+++ src/middleware/urls.ts
@@ -1,9 +1,9 @@
-import { getEnvironment } from "@src/utils/server/urls.ts";
+import { getEnvironment } from '@src/utils/server/urls.ts';
 
 const REDIRECT_URI = {
-  local: "http://localhost:4321/",
-  development: "https://sokos-utbetalingsportalen-astro.ansatt.dev.nav.no/",
-  production: "https://sokos-utbetalingsportalen-astro.ansatt.nav.no/",
+  local: 'http://localhost:4321/',
+  development: 'https://sokos-utbetalingsportalen-astro.ansatt.dev.nav.no/',
+  production: 'https://sokos-utbetalingsportalen-astro.ansatt.nav.no/',
 };
 
 export const redirectUri = REDIRECT_URI[getEnvironment()];
