b0f93e892d753514e306ba9ddd56b7afc27b4f7b
Mudasar Cheema <mudasar_cheema@hotmail.com>
2025-02-14 10:23:55 +0100

    Initial commit

---
+++ src/middleware/index.ts
@@ -0,0 +1,39 @@
+import { defineMiddleware } from 'astro/middleware';
+import { loginUrl } from './urls';
+import { isInternal } from './utils';
+import { isLocal } from '@src/utils/server/urls';
+import { getToken, validateToken } from '@navikt/oasis';
+
+export const onRequest = defineMiddleware(async (context, next) => {
+  const token = getToken(context.request.headers);
+  const params = encodeURIComponent(context.url.search);
+
+  if (isLocal) {
+    return next();
+  }
+
+  if (isInternal(context)) {
+    return next();
+  }
+
+  if (!token) {
+    console.info(
+      'Could not find any bearer token on the request. Redirecting to login.',
+    );
+    return context.redirect(`${loginUrl}${params}`);
+  }
+
+  const validation = await validateToken(token);
+
+  if (!validation.ok) {
+    const error = new Error(
+      `Invalid JWT token found (cause: ${validation.errorType} ${validation.error}, redirecting to login.`,
+    );
+    console.error(error);
+    return context.redirect(`${loginUrl}${params}`);
+  }
+
+  context.locals.token = token;
+
+  return next();
+});
---
+++ src/middleware/urls.ts
@@ -0,0 +1,10 @@
+import { getEnvironment } from "@src/utils/server/urls.ts";
+
+const REDIRECT_URI = {
+  local: "http://localhost:4321/",
+  development: "https://tms-astro-template.ansatt.dev.nav.no/",
+  production: "https://tms-astro-template.ansatt.nav.no/",
+};
+
+export const redirectUri = REDIRECT_URI[getEnvironment()];
+export const loginUrl = `/oauth2/login?redirect=${redirectUri}`;
