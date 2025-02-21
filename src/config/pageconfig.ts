f516d8825408026eadbf04bfb8ea47b08002f43e
Jonas Nicolaysen <jonas.nicolaysen@nav.no>
2025-02-21 22:07:57 +0100

    Prettier formattering

--- src/config/pageconfig.ts
+++ src/config/pageconfig.ts
@@ -1,18 +1,18 @@
 export const pagesConfig = [
-    {
-        app: 'ATTESTASJON',
-        title: 'Attestasjon',
-        description: 'Attestering av oppdrag',
-        adGroupDevelopment: '0de8d01f-8ad0-4391-841c-55392956bc17',
-        adGroupProduction: 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
-        url: 'attestasjon',
-    },
-    {
-        app: "OPPDRAGSINFO",
-        title: "Oppdragsinfo",
-        description: "Søk etter oppdrag i Oppdragssystemet",
-        adGroupDevelopment: "e0023d91-26bc-4d5d-95ba-3148b6123afc",
-        adGroupProduction: "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
-        url: "oppdragsinfo",
-    },
+  {
+    app: 'ATTESTASJON',
+    title: 'Attestasjon',
+    description: 'Attestering av oppdrag',
+    adGroupDevelopment: '0de8d01f-8ad0-4391-841c-55392956bc17',
+    adGroupProduction: 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
+    url: 'attestasjon',
+  },
+  {
+    app: 'OPPDRAGSINFO',
+    title: 'Oppdragsinfo',
+    description: 'Søk etter oppdrag i Oppdragssystemet',
+    adGroupDevelopment: 'e0023d91-26bc-4d5d-95ba-3148b6123afc',
+    adGroupProduction: 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
+    url: 'oppdragsinfo',
+  },
 ];
