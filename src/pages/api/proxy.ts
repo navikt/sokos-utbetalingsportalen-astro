ca6702840644dbb46fc935fb51994d0e0889f6a5
Jonas Nicolaysen <jonas.nicolaysen@nav.no>
2025-02-21 16:43:06 +0100

    Formattering

--- src/pages/api/proxy.ts
+++ src/pages/api/proxy.ts
@@ -1,10 +1,10 @@
 import type { APIRoute } from 'astro';
-import {getOboToken} from "../../utils/server/token.ts";
+import { getOboToken } from '../../utils/server/token.ts';
 
 export const GET: APIRoute = async function get(context) {
   console.log('Hello from proxy');
 
-  const oboToken = getOboToken("");
+  const oboToken = getOboToken('');
   // TODO: get oboToken
   // TODO: get correct url
   return context.rewrite('/attestasjon');
