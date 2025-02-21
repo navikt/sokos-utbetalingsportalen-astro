215880805410ec4adf3f2b25ea6b00327f10b013
mudasar187 <mudasar_cheema@hotmail.com>
2025-02-21 14:16:18 +0100

    test

--- src/pages/api/proxy.ts
+++ src/pages/api/proxy.ts
@@ -1,9 +1,11 @@
 import type { APIRoute } from 'astro';
+import {getOboToken} from "../../utils/server/token.ts";
 
 export const GET: APIRoute = async function get(context) {
-    console.log('Hello from proxy');
+  console.log('Hello from proxy');
 
-    // TODO: get oboToken
-    // TODO: get correct url
-    return context.rewrite("/attestasjon");
+  const oboToken = getOboToken("");
+  // TODO: get oboToken
+  // TODO: get correct url
+  return context.rewrite('/attestasjon');
 };
