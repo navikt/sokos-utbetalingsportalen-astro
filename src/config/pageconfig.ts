dcb1f877b9bfce1d98cfe17d22786cb5d350d130
Jonas Nicolaysen <jonas.nicolaysen@nav.no>
2025-02-23 17:19:07 +0100

    Fiks AppCard logikk og styling

--- src/config/pageconfig.ts
+++ src/config/pageconfig.ts
@@ -8,11 +8,27 @@ export const pagesConfig = [
     url: 'attestasjon',
   },
   {
+    app: 'TEMPLATE',
+    title: 'Grensesnittmal',
+    description: 'Dette er en blank mal for et grensesnitt',
+    adGroupDevelopment: '1b717a23-d376-471c-9fc6-356299fadc2b',
+    adGroupProduction: '',
+    url: 'template',
+  },
+  {
     app: 'OPPDRAGSINFO',
     title: 'Oppdragsinfo',
     description: 'Søk etter oppdrag i Oppdragssystemet',
     adGroupDevelopment: 'e0023d91-26bc-4d5d-95ba-3148b6123afc',
     adGroupProduction: 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
     url: 'oppdragsinfo',
+  },
+  {
+    app: 'VENTEREGISTER',
+    title: 'Venteregister',
+    description: 'Venteregister for oppdrag',
+    adGroupDevelopment: '48a80bbb-be45-4ef6-aab8-21604f057f47',
+    adGroupProduction: 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
+    url: 'venteregister',
   },
 ];
