7cf65feaaf6e64307ef460dd158043286014a813
mudasar187 <mudasar_cheema@hotmail.com>
2025-02-28 11:25:47 +0100

    legge inn riktig audience

--- src/pages/oppdrag-api/[...proxy].ts
+++ src/pages/oppdrag-api/[...proxy].ts
@@ -6,28 +6,29 @@ import { getOboToken } from 'src/utils/server/token';
 // Rewrite url til ny backend (fra config)
 // Returnere svar fra et api
 
-
 const getProxyUrl = (request: Request) => {
-    const url = request.url.replace('/oppdrag-api/*', '/api/v1/attestasjon');
-    console.log('url', url);
-    return new URL('https://sokos-oppdrag.dev-fss-pub.nais.io/api/v1/attestasjon'); // url fra config
+  const url = request.url.replace('/oppdrag-api/*', '/api/v1/attestasjon');
+  console.log('url', url);
+  return new URL(
+    'https://sokos-oppdrag.dev-fss-pub.nais.io/api/v1/attestasjon',
+  ); // url fra config
 };
 
 export const ALL: APIRoute = async (context: APIContext) => {
-    const audience = 'api://dev.fss.okonomi.sokos-oppdrag/.default';
-    const token = getOboToken(context.locals.token, audience); // audience fra config
-    const proxyUrl = getProxyUrl(context.request);
-    console.info('Proxying request to', proxyUrl.href);
+  const audience = 'api://dev.fss.okonomi.sokos-oppdrag/.default';
+  const token = getOboToken(context.locals.token, audience); // audience fra config
+  const proxyUrl = getProxyUrl(context.request);
+  console.info('Proxying request to', proxyUrl.href);
 
-    const response = await fetch(proxyUrl.href, {
-        method: context.request.method,
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${token}`,
-        },
-        body: context.request.body,
-    });
-    return new Response(response.body);
+  const response = await fetch(proxyUrl.href, {
+    method: context.request.method,
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+    body: context.request.body,
+  });
+  return new Response(response.body);
 };
 
 //https://sokos-utbetalingsportalen-astro.intern.dev.nav.no/api/[akjsdfkjansdf]
