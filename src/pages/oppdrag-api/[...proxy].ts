12510f629ee99d7f4572fa46f760dc7bbea37c98
mudasar187 <mudasar_cheema@hotmail.com>
2025-02-28 12:50:57 +0100

    fjenrer application/json

--- src/pages/oppdrag-api/[...proxy].ts
+++ src/pages/oppdrag-api/[...proxy].ts
@@ -7,13 +7,15 @@ import { getOboToken } from 'src/utils/server/token';
 // Returnere svar fra et api
 
 const getProxyUrl = (request: Request) => {
-  const url = request.url.replace(
-    'https://sokos-utbetalingsportalen-astro.intern.dev.nav.no/oppdrag-api',
-    'https://sokos-oppdrag.dev-fss-pub.nais.io',
-  );
-  console.log('url', url);
+    const requestUrl = request.url;
 
-  return new URL(url);
+    const url = request.url.replace(
+        'https://sokos-utbetalingsportalen-astro.intern.dev.nav.no/oppdrag-api',
+        'https://sokos-oppdrag.dev-fss-pub.nais.io',
+    );
+    console.log('url', url);
+
+    return new URL(url);
 };
 
 // https://sokos-utbetalingsportalen-astro.intern.dev.nav.no/oppdrag-api/api/v1/attestasjon/fagomraader
@@ -23,20 +25,19 @@ const getProxyUrl = (request: Request) => {
 // https://sokos-oppdrag.dev-fss-pub.nais.io/api/v1/oppdragsinfo/faggrupper
 
 export const ALL: APIRoute = async (context: APIContext) => {
-  const audience = 'api://dev-fss.okonomi.sokos-oppdrag/.default';
-  const token = getOboToken(context.locals.token, audience); // audience fra config
-  const proxyUrl = getProxyUrl(context.request);
-  console.info('Proxying request to', proxyUrl.href);
+    const audience = 'api://dev-fss.okonomi.sokos-oppdrag/.default';
+    const token = getOboToken(context.locals.token, audience); // audience fra config
+    const proxyUrl = getProxyUrl(context.request);
+    console.info('Proxying request to', proxyUrl.href);
 
-  const response = await fetch(proxyUrl.href, {
-    method: context.request.method,
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${token}`,
-    },
-    body: context.request.body,
-  });
-  return new Response(response.body);
+    const response = await fetch(proxyUrl.href, {
+        method: context.request.method,
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+        body: context.request.body,
+    });
+    return new Response(response.body);
 };
 
 //https://sokos-utbetalingsportalen-astro.intern.dev.nav.no/api/[akjsdfkjansdf]
