1cc437e7765d64cc7e12abc5eae416c9bc16f83b
Jonas Nicolaysen <jonas.nicolaysen@nav.no>
2025-03-06 13:20:54 +0100

    prettier

--- src/components/skeleton/AppSwitcherHeaderSkeleton.tsx
+++ src/components/skeleton/AppSwitcherHeaderSkeleton.tsx
@@ -1,9 +1,9 @@
-import { InternalHeader, Loader } from "@navikt/ds-react";
+import { InternalHeader, Loader } from '@navikt/ds-react';
 
-export default function AppSwitcherHeaderSkeleton () {
-    return (
-        <InternalHeader.Button>
-            <Loader variant="inverted" />
-        </InternalHeader.Button>
-    )
+export default function AppSwitcherHeaderSkeleton() {
+  return (
+    <InternalHeader.Button>
+      <Loader variant="inverted" />
+    </InternalHeader.Button>
+  );
 }
