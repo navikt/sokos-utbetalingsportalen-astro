3b9ddc7482cb8eaec9264774d0b5b8be9801955b
Jonas Nicolaysen <jonas.nicolaysen@nav.no>
2025-03-11 14:35:19 +0100

    Bruk loader skeleton

--- src/components/skeleton/SideBarSkeleton.tsx
+++ src/components/skeleton/SideBarSkeleton.tsx
@@ -1,21 +1,11 @@
-import { Skeleton } from '@navikt/ds-react';
+import { Loader } from '@navikt/ds-react';
 import styles from '../sidebar/SideBar.module.css';
 
 export default function SideBarSkeleton() {
   return (
     <div className={`${styles.closed} ${styles.sidebar}`} role="navigation">
-      <div style={{ padding: '0.5rem' }}>
-        <Skeleton
-          variant="rectangle"
-          width="2rem"
-          height="2rem"
-          style={
-            {
-              '--ac-skeleton-bg': 'rgba(255, 255, 255, 0.3)',
-              '--ac-skeleton-shine': 'rgba(255, 255, 255, 0.6)',
-            } as React.CSSProperties
-          }
-        />
+      <div style={{ padding: '0.7rem' }}>
+        <Loader variant="inverted" />
       </div>
     </div>
   );
