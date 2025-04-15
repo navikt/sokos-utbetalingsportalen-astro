3166ed4c686afd1fd2a76710386987b7d249db40
mudasar187 <mudasar_cheema@hotmail.com>
2025-04-15 14:06:58 +0200

    tester

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,4 +1,13 @@
 export default {
   extends: ['stylelint-config-standard', '@navikt/aksel-stylelint/recommended'],
+  rules: {
+    'selector-class-pattern': null,
+    'selector-pseudo-class-no-unknown': [
+      true,
+      {
+        ignorePseudoClasses: ['global'],
+      },
+    ],
+  },
   ignoreFiles: 'dist/**/*',
 };
